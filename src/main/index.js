8e6805a3f09185796da4ebfc21dbdee0c8472f9b
mijdavis2 <mdavisinsc@gmail.com>
2017-07-12 23:25:42 -0700

    Add menu items

--- src/main/index.js
+++ src/main/index.js
@@ -19,13 +19,22 @@ function createWindow () {
   /**
    * Initial window options
    */
+  const shouldShow = true
   mainWindow = new BrowserWindow({
     height: 1000,
     useContentSize: true,
-    width: 1434
+    width: 1434,
+    show: false
   })
 
   mainWindow.loadURL(winURL)
+
+  // workaround to fix weird scroll behavior
+  mainWindow.on('ready-to-show', () => {
+    if (shouldShow) {
+      mainWindow.show()
+    }
+  })
 
   mainWindow.on('closed', () => {
     mainWindow = null
@@ -45,3 +54,93 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+const { Menu } = require('electron')
+
+const template = [
+  {
+    label: 'Edit',
+    submenu: [
+      {role: 'undo'},
+      {role: 'redo'},
+      {type: 'separator'},
+      {role: 'cut'},
+      {role: 'copy'},
+      {role: 'paste'},
+      {role: 'pasteandmatchstyle'},
+      {role: 'delete'},
+      {role: 'selectall'}
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {role: 'reload'},
+      {role: 'forcereload'},
+      {role: 'toggledevtools'},
+      {type: 'separator'},
+      {role: 'resetzoom'},
+      {role: 'zoomin'},
+      {role: 'zoomout'},
+      {type: 'separator'},
+      {role: 'togglefullscreen'}
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      {role: 'minimize'},
+      {role: 'close'}
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click () { require('electron').shell.openExternal('https://electron.atom.io') }
+      }
+    ]
+  }
+]
+
+if (process.platform === 'darwin') {
+  template.unshift({
+    label: app.getName(),
+    submenu: [
+      {role: 'about'},
+      {type: 'separator'},
+      {role: 'services', submenu: []},
+      {type: 'separator'},
+      {role: 'hide'},
+      {role: 'hideothers'},
+      {role: 'unhide'},
+      {type: 'separator'},
+      {role: 'quit'}
+    ]
+  })
+
+  // Edit menu
+  template[1].submenu.push(
+    {type: 'separator'},
+    {
+      label: 'Speech',
+      submenu: [
+        {role: 'startspeaking'},
+        {role: 'stopspeaking'}
+      ]
+    }
+  )
+
+  // Window menu
+  template[3].submenu = [
+    {role: 'close'},
+    {role: 'minimize'},
+    {role: 'zoom'},
+    {type: 'separator'},
+    {role: 'front'}
+  ]
+}
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
